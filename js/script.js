b0cc7eecb219dde5ea157e5630740cbd91699122
Marcos Júnior <marcos_lopes_ribeiro_junior@hotmail.com>
2021-07-25 02:41:55 -0300

    MenuMobile refatorado

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ import Tooltip from "./modules/tooltip.js";
 import Modal from "./modules/modal.js";
 import ScrollAnima from "./modules/scroll-anima.js";
 import DropdownMenu from "./modules/dropdown-menu.js";
-import initMenuMobile from "./modules/menu-mobile.js";
+import MenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetch-animais.js";
 import fetchBitcoin from "./modules/fetch-bitcoin.js";
@@ -41,7 +41,9 @@ const dropdownMenu = new DropdownMenu("[data-dropdown]", [
 ]);
 dropdownMenu.init();
 
-initMenuMobile();
+const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
+menuMobile.init();
+
 initFuncionamento();
 
 fetchAnimais("../../animaisapi.json", ".numeros-grid");

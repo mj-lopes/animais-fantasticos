fb7a365bd2b97458162b4e30b50ff1e1a00b362a
Marcos Júnior <marcos_lopes_ribeiro_junior@hotmail.com>
2021-07-29 00:13:15 -0300

    Adicionado slide de fotos

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ import MenuMobile from "./modules/menu-mobile.js";
 import Funcionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetch-animais.js";
 import fetchBitcoin from "./modules/fetch-bitcoin.js";
+import SlideNav from "./modules/slide.js";
 
 const scroll = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scroll.init();
@@ -49,3 +50,8 @@ funcionamento.init();
 
 fetchAnimais("./animaisapi.json", ".numeros-grid");
 fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
+
+const slide = new SlideNav(".slide", ".slide-wrapper");
+slide.init();
+
+slide.addControl(".custom-controls");
